c7041c486ef20e843c8bf42ac83bb95aaa56a229
Shreya Singh <66567410+Shreyav23Singh@users.noreply.github.com>
2023-04-22 14:53:35 +0530

    Update book_app.js

--- book_app.js
+++ book_app.js
@@ -1,1 +1,47 @@
------js -----
+function saveToLocalStorage(event){
+    event.preventDefault();
+    console.log("hi")
+    
+    const firstName =event.target.fname.value;
+    const lastName =event.target.lname.value;
+    const email =event.target.email.value;
+    const phoneNo =event.target.phone.value;
+    const Time =event.target.time.value;
+    const Date =event.target.date.value;
+    
+
+
+const obj ={
+    firstName,
+    lastName ,
+    email,
+    phoneNo,
+    Time,
+    Date
+}
+console.log(obj)
+
+
+localStorage.setItem(obj.email , JSON.stringify(obj))
+
+axios.post("https://crudcrud.com/api/18369d96034e4dfaa559786e119ffa46/AppointmentData" ,obj)
+.then((response)=>{
+    console.log(response)
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+}
+// window.addEventListener("DOMContentLoaded" ,()=>{
+//     const LocalStorageObj =localStorage;
+//     const localstoragekeys=Object.keys(LocalStorageObj)
+
+//     for(var i=0 ; i<localstoragekeys.length ; i++){
+//         const key=localstoragekeys[i];
+//         const userDetailstring =LocalStorageObj[key];
+//         const userDetailsobj = JSON.parse(userDetailstring);
+
+//     }
+
+// })
